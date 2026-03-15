db771ff3b5ce3597de4c7deaef53f88a641cb139
balaji <Balaji.pasupuleti931@gmail.com>
2026-03-15 23:58:53 +0530

    day1

---
+++ document1/one.js
@@ -0,0 +1,39 @@
+//this is number type
+let a=10
+let b=20
+console.log(a+b);
+
+//this is string type
+let aa="allu"
+let bb="arjun"
+console.log(aa+bb);
+
+//this is boolean type
+let bool=true
+let bool1=false
+
+console.log(bool)
+
+//undefined
+
+let result;
+console.log(result);
+
+//null type
+
+let num=null
+console.log(num);
+console.log(typeof(num)); //here typeof null is obj  its a js bug eventhough its valid
+
+//bigint
+
+let bi=BigInt(12345678910)
+console.log(bi);
+console.log(typeof(bi));
+
+
+let bin=(123456789n)
+console.log(bin);
+console.log(typeof(bin));
+
+
